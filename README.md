d569241f11c5f9ac16d1b2ac4b2e1b7c9ac316b1
Wai Loon <wai.loon.theng@accenture.com>
2019-12-01 22:04:26 +0800

    Updated code to expose Lambda function via AWS API Gateway

---
+++ README.md
@@ -0,0 +1,20 @@
+# lambda-uppercase
+This is a sample code for Spring Cloud Function with AWS Lambda adapter.
+
+## Getting Started
+
+### Prerequisite
+1. Create an [execution role](https://docs.aws.amazon.com/lambda/latest/dg/lambda-intro-execution-role.html) for AWS Lambda.
+
+### Create Lambda Function (expose to AWS API Gateway)
+Use `org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler` when using AWS API Gateway as Lambda trigger. 
+> Replace `[USERID]` and `[ROLE]` with your corresponding AWS details.
+
+```shell script
+aws lambda create-function --function-name lambda-uppercase --role arn:aws:iam::[USERID]:role/[ROLE] --zip-file fileb://target/lambda-uppercase-0.0.1-SNAPSHOT-aws.jar --handler org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler --description "Spring Cloud Function AWS Lambda Example" --runtime java11 --region ap-southeast-1 --timeout 30 --memory-size 1024 --publish
+```
+
+### Update Lambda Function
+```shell script
+mvn -U clean package -DskipTests && aws lambda update-function-code --function-name lambda-uppercase --zip-file fileb://target/lambda-uppercase-0.0.1-SNAPSHOT-aws.jar --publish
+```
--- src/main/java/com/example/lambdauppercase/LambdaUppercaseApplication.java
+++ src/main/java/com/example/lambdauppercase/LambdaUppercaseApplication.java
@@ -2,22 +2,50 @@ package com.example.lambdauppercase;
 
 import java.util.function.Function;
 
-import reactor.core.publisher.Flux;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionRegistration;
+import org.springframework.cloud.function.context.FunctionType;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
-public class LambdaUppercaseApplication {
+public class LambdaUppercaseApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
 	@Bean
-	public Function<Flux<String>, Flux<String>> uppercase() {
-		return flux -> flux.map(String::toUpperCase);
+	public Function<Foo, Bar> uppercase() {
+		return v -> new Bar("Hello " + v.getMessage().toUpperCase());
+	}
+
+	@Override
+	public void initialize(GenericApplicationContext context) {
+		context.registerBean("uppercase", FunctionRegistration.class,
+				() -> new FunctionRegistration<>(uppercase())
+						.type(FunctionType.from(Foo.class).to(Bar.class)));
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaUppercaseApplication.class, args);
 	}
 
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Foo {
+	private String message;
+}
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Bar {
+	private String value;
 }
